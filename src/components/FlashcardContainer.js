8e2b693a445785b96e635d274b303e09c2c94c9f
aspittel <ali07cat07@gmail.com>
2017-09-29 12:37:55 -0400

    in class version

--- src/components/FlashcardContainer.js
+++ src/components/FlashcardContainer.js
@@ -5,46 +5,10 @@ import {CLIENT_URL} from '../constants.js'
 import FlashcardDetail from './FlashcardDetail'
 
 class FlashcardContainer extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        flashcards: [],
-        show: false,
-        currentIndex: 0
-      }
-    }
-
-    next () {
-      if (this.state.show) 
-        this.setState(currState => ({currentIndex: currState.currentIndex + 1}))
-      this.setState(currState => ({show: !currState.show}))
-    }
-
-    componentDidMount () {
-      window.addEventListener('keyup', (event) => {
-        // move to next card on right arrow
-        if (event.keyCode === 39) 
-          this.next()
-      })
-
-      axios
-        .get(`${CLIENT_URL}/api/words`)
-        .then(response => this.setState({flashcards: response.data}))
-        .catch(err => console.log(err))
-    }
     
     render() {
-      let flashcard = this.state.flashcards[this.state.currentIndex]
       return (
         <div>
-            <main>
-              <div className="container">
-                {flashcard && 
-                <FlashcardDetail
-                  card={flashcard}
-                  show={this.state.show}/>}
-              </div>
-            </main>
         </div>
       )
     }
