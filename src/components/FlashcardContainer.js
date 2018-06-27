07ee3d59299bfdcf4ef536d3c16ce7f75566d33d
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2018-06-27 13:46:30 -0400

    swap out axios for fetch

--- src/components/FlashcardContainer.js
+++ src/components/FlashcardContainer.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react'
-import Flashcard from './Flashcard'
-import axios from 'axios'
-import { CLIENT_URL } from '../constants.js'
+import React, { Component } from "react";
+import Flashcard from "./Flashcard";
+import { CLIENT_URL } from "../constants.js";
 
 class FlashcardContainer extends Component {
-
   render() {
-    return (
-      <div>
-      </div>
-    )
+    return <div />;
   }
 }
 
-export default FlashcardContainer
+export default FlashcardContainer;
