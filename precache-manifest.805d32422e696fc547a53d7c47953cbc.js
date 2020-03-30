8d8ed5260989df7c116bc0ba20172dd1f58e870a
Min Su Kim <kim97minsu@gmail.com>
2020-03-30 13:32:30 -0700

    Updates

---
+++ precache-manifest.805d32422e696fc547a53d7c47953cbc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3699ae5df2d165e607f3d688995ce27d",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "445e63c52bb5605beaea",
+    "url": "/flashcards/static/js/2.085b0623.chunk.js"
+  },
+  {
+    "revision": "3aee493e1ecd6fa9ac1b",
+    "url": "/flashcards/static/js/main.a00a0ae7.chunk.js"
+  },
+  {
+    "revision": "b83ba50f0a9375b69a4b",
+    "url": "/flashcards/static/js/runtime~main.45c4c180.js"
+  }
+]);
